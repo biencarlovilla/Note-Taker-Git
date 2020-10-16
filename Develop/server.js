d3cdd9800ebff10578de35e9baeef52bd687e712
Biencarlo Villa <vbiencarlo_29@yahoo.com>
2020-10-16 16:42:58 -0700

    updated server.js file and added api.js and html.js

--- Develop/server.js
+++ Develop/server.js
@@ -3,3 +3,14 @@ var path = require("path");
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./api")(app);
+require("./html")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
